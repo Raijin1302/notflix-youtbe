b1822d9c7d3e200af26be3d25e46b9c8113d89fc
Raijin1302 <raijinnguyen1302@gmail.com>
2023-05-18 23:40:16 +0700

    :red_circle: :hammer: build Input

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,41 @@
-import { FC } from "react"
+"use client"
+import Input from "@/components/Input"
+import { FC, useState } from "react"
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
+  const [email, setEmail] = useState<string>("")
+  const [username, setUsername] = useState<string>("")
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover">
-      <div className="bg-black w-full h-full lg:bg-opacity-50">
+      <div className="bg-black w-full h-full lg:bg-opacity-50 ">
         <nav className="px-12 py-5">
           <img src="/images/logo.png" alt="Logo" className="h-8" />
         </nav>
-        <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md w-full-">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+        <div className="flex justify-center ">
+          <div className="bg-black bg-opacity-70 px-16 py-16  mt-2 lg:w-2/5 lg:max-w-md w-full-">
+            <h2 className="text-white text-4xl mb-8 font-semibold">Sign in </h2>
+            <div className="flex flex-col gap-4">
+              <Input
+                label="Email"
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setEmail(e.target.value)
+                }
+              />
+              <Input
+                label="Username"
+                id="username"
+                type="username"
+                value={username}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setUsername(e.target.value)
+                }
+              />
+            </div>
           </div>
         </div>
       </div>
---
+++ src/components/Input.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react"
+
+interface InputProps {
+  id: string
+  value: string
+  label: string
+  onChange: any
+  type?: string
+}
+
+const Input: FC<InputProps> = ({ id, onChange, value, label, type }) => {
+  return (
+    <div className="relative">
+      <input
+        onChange={onChange}
+        value={value}
+        type={type}
+        id={id}
+        className="block rounded-md px-6 pt-6 pb-1 w-full text-sm text-white bg-neutral-700 app
+      focus:outline-none focus:ring-0 peer"
+        placeholder=" "
+      />
+      <label
+        htmlFor={id}
+        className="absolute text-sm text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3"
+      >
+        {label}
+      </label>
+    </div>
+  )
+}
+
+export default Input
