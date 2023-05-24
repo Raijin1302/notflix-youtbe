2c3a1924bf478783ea30d2b77c087509f7bb96ee
Raijin1302 <raijinnguyen1302@gmail.com>
2023-05-24 23:45:50 +0700

    :green_circle: :hammer: update github login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,8 +2,54 @@ import { db } from "@/lib/db"
 import { NextAuthOptions } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import { compare } from "bcrypt"
+import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+
+// function getGoogleCredentials(): { clientId: string; clientSecret: string } {
+//   const clientId = process.env.GOOGLE_CLIENT_ID
+//   const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+//   if (!clientId || clientId.length === 0) {
+//     throw new Error(
+//       "Missing GOOGLE_CLIENT_ID - No clientID for google provider set"
+//     )
+//   }
+//   if (!clientSecret || clientSecret.length === 0) {
+//     throw new Error(
+//       "Missing GOOGLE_CLIENT_SECRET - No clientSecret for google provider set"
+//     )
+//   }
+
+//   return { clientId, clientSecret }
+// }
+
+// function getGitHubCredentials(): { clientId: string; clientSecret: string } {
+//   const clientId = process.env.GITHUB_CLIENT_ID
+//   const clientSecret = process.env.GITHUB_CLIENT_SECRET
+//   if (!clientId || clientId.length === 0) {
+//     throw new Error(
+//       "Missing GITHUB_CLIENT_ID - No clientID for google provider set"
+//     )
+//   }
+//   if (!clientSecret || clientSecret.length === 0) {
+//     throw new Error(
+//       "Missing GITHUB_CLIENT_SECRET - No clientSecret for google provider set"
+//     )
+//   }
+
+//   return { clientId, clientSecret }
+// }
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
     Credentials({
       id: "credentials",
       name: "Credentials",
