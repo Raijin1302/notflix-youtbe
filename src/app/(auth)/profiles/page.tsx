6e105224300d87513848456ffa8dc1946a32971d
Raijin1302 <raijinnguyen1302@gmail.com>
2023-05-30 23:15:27 +0700

    :red_circle: building profiles page

--- src/app/(auth)/profiles/page.tsx
+++ src/app/(auth)/profiles/page.tsx
@@ -1,13 +1,34 @@
+import LgHeading from "@/components/ui/LgHeading"
 import { authOptions } from "@/lib/auth"
 import { getServerSession } from "next-auth"
+import Link from "next/link"
 import { redirect } from "next/navigation"
-
 const page = async () => {
   const session = await getServerSession(authOptions)
   if (!session) {
     return redirect("/login")
   } else {
-    return <div className="text-white">Profiles</div>
+    return (
+      <div className="flex items-center h-full justify-center">
+        <div className="flex flex-col">
+          <LgHeading className="text-white text-center">
+            Who is watching ?
+          </LgHeading>
+          <div className="flex items-center justify-center gap-8 mt-10">
+            <Link href="/">
+              <div className="group flex-row w-44 mx-auto">
+                <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
+                  <img src="/images/default-green.png" alt="Profile" />
+                </div>
+                <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
+                  {session?.user?.name}
+                </div>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { getServerSession } from "next-auth"
 import { signOut } from "next-auth/react"
 export default async function Home() {
   const session = await getServerSession(authOptions)
-  console.log(session)
 
   return (
     <main>
