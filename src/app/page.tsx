b500fade058f1c66438ed448e5d61e3059827744
Raijin1302 <raijinnguyen1302@gmail.com>
2023-06-05 23:40:12 +0700

    :red_circle: :hammer: build Billboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,23 +5,14 @@ import { authOptions } from "@/lib/auth"
 import { getServerSession } from "next-auth"
 import { signOut } from "next-auth/react"
 export default async function Home() {
-  const session = await getServerSession(authOptions)
+  // const session = await getServerSession(authOptions)
 
   return (
     <main>
-      <h1 className="text-3xl text-red-700">Notflix Clone</h1>
-      {session ? (
-        <>
-          <SignOutButton />
-          <p className="text-green-700">
-            Logged in as : {session?.user?.email}
-          </p>
-          <Navbar />
-          <Billboard />
-        </>
-      ) : (
-        <p className="text-green-400">Nothing in here</p>
-      )}
+      <>
+        <Navbar />
+        <Billboard />
+      </>
     </main>
   )
 }
