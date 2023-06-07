9133cf5919df013ac466ca1d48d8bf3be258c8b5
Raijin1302 <raijinnguyen1302@gmail.com>
2023-06-07 23:46:37 +0700

    :red_cỉccle: :hammer: update MovieList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
+"use client"
 import Billboard from "@/components/Billboard"
+import MovieList from "@/components/MovieList"
 import Navbar from "@/components/Navbar"
 import SignOutButton from "@/components/SignOutButton"
+import useMovieList from "@/hooks/useMovieList"
 import { authOptions } from "@/lib/auth"
 import { getServerSession } from "next-auth"
 import { signOut } from "next-auth/react"
-export default async function Home() {
+export default function Home() {
   // const session = await getServerSession(authOptions)
-
+  const { data: movies = [] } = useMovieList()
   return (
     <main>
       <>
         <Navbar />
         <Billboard />
+        <div className="pb-40">
+          <MovieList title="Trending Now" data={movies} />
+        </div>
       </>
     </main>
   )
---
+++ src/components/MovieList.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react"
+import { isEmpty } from "lodash"
+import LgHeading from "./ui/LgHeading"
+import MovieCard from "./MovieCard"
+
+interface MovieListProps {
+  data: Record<string, any>[]
+  title: string
+}
+
+const MovieList: FC<MovieListProps> = ({ data, title }) => {
+  if (isEmpty(data)) {
+    return null
+  }
+
+  return (
+    <div className="px-4 md:px-12 mt-4 space-y-8">
+      <div>
+        <LgHeading size="xs" className="text-white font-semibold">
+          {title}
+        </LgHeading>
+        <div className="grid grid-cols-4 gap-2">
+          {data.map((movie) => (
+            <MovieCard key={movie.id} data={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MovieList
