11c5b2c7b95631d466ec382573b666f288214050
Raijin1302 <raijinnguyen1302@gmail.com>
2023-06-16 00:06:54 +0700

    :green_circle: :hammer: done build InfoModal

--- src/hooks/useInfoModalStore.ts
+++ src/hooks/useInfoModalStore.ts
@@ -6,11 +6,11 @@ export interface ModalStoreInterface {
   closeModal: () => void
 }
 
-const useInfoModal = create<ModalStoreInterface>((set) => ({
+const useInfoModalStore = create<ModalStoreInterface>((set) => ({
   movieId: undefined,
   isOpen: false,
   openModal: (movieId: string) => set({ isOpen: true, movieId }),
   closeModal: () => set({ isOpen: false, movieId: undefined }),
 }))
 
-export default useInfoModal
+export default useInfoModalStore
